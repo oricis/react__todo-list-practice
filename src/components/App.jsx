1280c86174f48817d994e8da1f486ca80e377975
orici <m_alc1@hotmail.com>
2019-10-15 15:53:47 +0200

    extracted JSX to component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from '../assets/images/logo.svg';
 import '../assets/styles/App.css';
+import React from 'react';
+import TaskList from "./task-list/TaskList";
+import logo from '../assets/images/logo.svg';
 
 function App() {
   return (
@@ -11,25 +12,7 @@ function App() {
         <h3>TODO List</h3>
       </header>
 
-      <section class="App-body">
-        <article>
-            <label htmlFor="create-task">Introduce una tarea:</label>
-            <input type="text" id="create-task" />
-        </article>
-
-        <article class="task-list">
-            <div class="task">
-                <p class="task-title">Foo</p>
-                <input type="button" class="btn" value="Completada" />
-                <input type="button" class="btn" value="Eliminar" />
-            </div>
-            <div class="task">
-                <p class="task-title">Lorem ipsum</p>
-                <input type="button" class="btn" value="Completada" />
-                <input type="button" class="btn" value="Eliminar" />
-            </div>
-        </article>
-      </section>
+      <TaskList></TaskList>
     </div>
   );
 }
---
+++ src/components/task-list/TaskList.jsx
@@ -0,0 +1,42 @@
+import './task-list.css';
+import React, { Component } from 'react';
+
+class TaskList extends Component
+{
+    constructor(props)
+    {
+        super(props);
+
+        // Set default values
+        this.state = {
+
+        };
+    }
+
+    render()
+    {
+        return (
+            <section class="App-body">
+                <article>
+                    <label htmlFor="create-task">Introduce una tarea:</label>
+                    <input type="text" id="create-task" />
+                </article>
+
+                <article class="task-list">
+                    <div class="task">
+                        <p class="task-title">Foo</p>
+                        <input type="button" class="btn" value="Completada" />
+                        <input type="button" class="btn" value="Eliminar" />
+                    </div>
+                    <div class="task">
+                        <p class="task-title">Lorem ipsum</p>
+                        <input type="button" class="btn" value="Completada" />
+                        <input type="button" class="btn" value="Eliminar" />
+                    </div>
+                </article>
+            </section>
+        );
+    }
+}
+
+export default TaskList;

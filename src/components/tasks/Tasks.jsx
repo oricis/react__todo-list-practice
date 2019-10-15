c3a8503d67ae96b94d6404a3a1d5b176ff9d7369
orici <m_alc1@hotmail.com>
2019-10-15 17:52:15 +0200

    loaded task cards data from state

--- src/components/tasks/Tasks.jsx
+++ src/components/tasks/Tasks.jsx
@@ -1,29 +1,44 @@
 
 import './tasks.css';
 import React, { Component } from 'react';
+import generateID from '../../helpers/generateID.js';
 
-class Tasks extends Component {
-    constructor(props) {
+class Tasks extends Component
+{
+    constructor(props)
+    {
         super(props);
 
         // Set default values
         this.state = {
-
+            tasks: [
+                {
+                    text: 'Foo',
+                    completed: false,
+                    id: generateID(),
+                },
+                {
+                    text: 'Lorem ipsum',
+                    completed: true,
+                    id: generateID(),
+                },
+            ],
         };
     }
 
-    render() {
+    render()
+    {
         return (
             <article className="task-list">
-                <div className="content-box task">
-                    <p className="task-title">Foo</p>
+                <div className="content-box task" data-id={ this.state.tasks[0].id }>
+                    <p className="task-title">{ this.state.tasks[0].text }</p>
                     <div className="task-buttons">
                         <input type="button" className="btn btn-primary" value="Completada" />
                         <input type="button" className="btn btn-primary" value="Eliminar" />
                     </div>
                 </div>
-                <div className="content-box task">
-                    <p className="task-title">Lorem ipsum</p>
+                <div className="content-box task" data-id="{ this.state.tasks[1].id }">
+                    <p className="task-title">{ this.state.tasks[1].text }</p>
                     <div className="task-buttons">
                         <input type="button" className="btn btn-primary" value="Completada" />
                         <input type="button" className="btn btn-primary" value="Eliminar" />

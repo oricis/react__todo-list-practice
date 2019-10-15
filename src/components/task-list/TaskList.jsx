ed7a21e5717e874bbb783048830b84e6dd21bfc4
orici <m_alc1@hotmail.com>
2019-10-15 16:39:48 +0200

    fixed attrs: class -> className

--- src/components/task-list/TaskList.jsx
+++ src/components/task-list/TaskList.jsx
@@ -16,25 +16,25 @@ class TaskList extends Component
     render()
     {
         return (
-            <section class="App-task-list">
-                <article class="task-intro">
+            <section className="App-task-list">
+                <article className="task-intro">
                     <label htmlFor="create-task">Introduce una tarea:</label>
                     <input type="text" id="create-task" />
                 </article>
 
-                <article class="task-list">
-                    <div class="task">
-                        <p class="task-title">Foo</p>
+                <article className="task-list">
+                    <div className="task">
+                        <p className="task-title">Foo</p>
                         <div className="task-buttons">
-                            <input type="button" class="btn btn-primary" value="Completada" />
-                            <input type="button" class="btn btn-primary" value="Eliminar" />
+                            <input type="button" className="btn btn-primary" value="Completada" />
+                            <input type="button" className="btn btn-primary" value="Eliminar" />
                         </div>
                     </div>
-                    <div class="task">
-                        <p class="task-title">Lorem ipsum</p>
+                    <div className="task">
+                        <p className="task-title">Lorem ipsum</p>
                         <div className="task-buttons">
-                            <input type="button" class="btn btn-primary" value="Completada" />
-                            <input type="button" class="btn btn-primary" value="Eliminar" />
+                            <input type="button" className="btn btn-primary" value="Completada" />
+                            <input type="button" className="btn btn-primary" value="Eliminar" />
                         </div>
                     </div>
                 </article>

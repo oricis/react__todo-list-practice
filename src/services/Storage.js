df1520e5fa9c894f9be133b8cee96275c06ce3e5
oricis <m_alc1@hotmail.com>
2019-10-17 17:20:15 +0200

    added a class stakeholder to handle a basic data persistence

---
+++ src/services/Storage.js
@@ -0,0 +1,24 @@
+
+class Storage
+{
+
+    clear()
+    {
+
+    }
+
+    get(key)
+    {
+
+    }
+
+    set(key, value)
+    {
+        value = JSON.stringify(value);
+    }
+
+    unset(key)
+    {
+
+    }
+}

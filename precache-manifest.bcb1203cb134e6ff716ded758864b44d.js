a52842ff0ded4ca414755ef39711c7dd5acba360
moises.alcocer <contacto@ironwoods.es>
2019-12-02 12:28:00 +0100

    Updates

---
+++ precache-manifest.bcb1203cb134e6ff716ded758864b44d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f23662b2e5df882d08d47edebf69039",
+    "url": "/oricis/react__todo-list-practice/index.html"
+  },
+  {
+    "revision": "ac29aeb7d3437106ba2a",
+    "url": "/oricis/react__todo-list-practice/static/css/main.62c285a8.chunk.css"
+  },
+  {
+    "revision": "232ea90c7d3d6a5b51d7",
+    "url": "/oricis/react__todo-list-practice/static/js/2.6678072c.chunk.js"
+  },
+  {
+    "revision": "ac29aeb7d3437106ba2a",
+    "url": "/oricis/react__todo-list-practice/static/js/main.43182593.chunk.js"
+  },
+  {
+    "revision": "fc4ba9006f4966158ef6",
+    "url": "/oricis/react__todo-list-practice/static/js/runtime-main.406dbbf0.js"
+  },
+  {
+    "revision": "8cc8eac2df26843cac5e88f13628e29c",
+    "url": "/oricis/react__todo-list-practice/static/media/logo.8cc8eac2.svg"
+  }
+]);

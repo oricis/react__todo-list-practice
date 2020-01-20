eeb481bd699757646efb6a90ddc051c2c775327a
moises.alcocer <contacto@ironwoods.es>
2020-01-20 18:54:35 +0100

    added to UI button to swap tasks / lists

--- src/components/task-list/form/Form.jsx
+++ src/components/task-list/form/Form.jsx
@@ -1,11 +1,16 @@
 import './form.scss';
 import React, { Component } from 'react';
 import Storage from '../../../services/Storage.js';
+import swapIcon from '../../../assets/images/buffer-brands.svg';
 import PropTypes from 'prop-types';
+import ImageButton from '../../common/image-button';
 import TextButton from '../../common/text-button';
+import TextLabel from '../../common/text-label';
 
 class Form extends Component
 {
+    actualMode = 'tasks'; // tasks | lists
+
     constructor(props)
     {
         super(props);
@@ -27,27 +32,44 @@ class Form extends Component
     {
         return (
             <article className="content-box task-intro">
-                <input type="text"
-                    id="create-task"
-                    placeholder="Introduce una tarea..."
-                    value={this.state.newTaskText}
-                    onChange={event =>
-                        this.setState({ newTaskText: event.target.value })
-                    }
-                    onKeyUp={this.addTask} />
+                <div className="config-actions">
+                    <TextLabel
+                        text="Lorem ipsum">
+                    </TextLabel>
 
-                <TextButton
-                    className="btn"
-                    text="Añadir"
-                    onClick={this.addTaskFromInput}></TextButton>
-                <TextButton
-                    className="btn"
-                    text="Limpiar"
-                    onClick={this.cleanTaskInput}></TextButton>
+                    <ImageButton
+                        className="image-btn"
+                        image={swapIcon}
+                        attrAlt={"Cambiar modo a " + this.actualMode}
+                        attrTitle={"Cambiar modo a " + this.actualMode}
+                        onClick={this.clickedSwapButton}>
+                    </ImageButton>
+                </div>
+                <div className="creation-form">
+                    <input type="text"
+                        id="create-task"
+                        placeholder="Introduce una tarea..."
+                        value={this.state.newTaskText}
+                        onChange={event =>
+                            this.setState({ newTaskText: event.target.value })
+                        }
+                        onKeyUp={this.addTask} />
+
+                    <TextButton
+                        className="btn"
+                        text="Añadir"
+                        onClick={this.addTaskFromInput}></TextButton>
+                    <TextButton
+                        className="btn"
+                        text="Limpiar"
+                        onClick={this.cleanTaskInput}></TextButton>
+                </div>
             </article>
         );
     }
-
+    clickedSwapButton = () => {
+        console.log('clickedSwapButton()')
+    }
     /**
      * Custom methods
      * Create task form actions

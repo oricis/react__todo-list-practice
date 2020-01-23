e228a430364f364902239b69a5e15a6cb0cb0ca5
moises.alcocer <contacto@ironwoods.es>
2020-01-23 14:36:38 +0100

    duplicated component Task in List

---
+++ src/components/todo/cards/list/card-header/CardHeader.jsx
@@ -0,0 +1,37 @@
+
+import './styles.scss';
+import React, { Component } from 'react';
+import editIcon from '../../../../../assets/images/edit-regular.svg';
+import ColorPicket from '../../../../common/color-picket/ColorPicket';
+import ImageButton from '../../../../common/image-button';
+
+class CardHeader extends Component
+{
+
+    render()
+    {
+        const editButton = (this.props.completed)
+            ? ''
+            : <ImageButton
+                className="image-btn"
+                image={editIcon}
+                attraAlt="Editar"
+                attrtTitle="Editar"
+                onClick={this.props.onClickToEdit}>
+            </ImageButton>;
+
+        return (
+            <div className="card-header">
+                {editButton}
+
+                <ColorPicket
+                    bgColor={this.props.bgColor}
+                    completed={this.props.completed}
+                    setColorFromPicket={this.props.setColorFromPicket}>
+                </ColorPicket>
+            </div>
+        );
+    }
+}
+
+export default CardHeader;

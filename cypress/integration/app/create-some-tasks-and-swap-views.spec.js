bd1ceb9386ee02ac2b45080b3647037424988d6b
moises.alcocer <contacto@ironwoods.es>
2020-09-19 00:43:42 +0200

    cypress: create tasks & go back to lists

---
+++ cypress/integration/app/create-some-tasks-and-swap-views.spec.js
@@ -0,0 +1,53 @@
+/// <reference types="cypress" />
+
+describe('Swap between the app\'s modes: lists and tasks views', () => {
+  it('Opening the ' + Cypress.config().appName + '...', () => {
+
+    cy.visit(Cypress.config().baseUrl)
+  })
+
+  it('Add lists', () => {
+
+    cy.get('#input-title')
+      .type('fake list')
+    cy.contains('.btn', 'Añadir')
+      .click()
+    cy.get('#input-title')
+      .clear()
+  })
+
+  it('Moving to tasks view...', () => {
+
+      cy.get('.config-actions > .image-btn > img')
+        .click()
+  })
+
+  it('Adding tasks...', () => {
+    cy.get('#input-title')
+        .type('First task')
+    cy.contains('.btn', 'Añadir')
+        .click()
+    cy.get('#input-title')
+        .clear()
+
+    cy.get('#input-title')
+        .type('My second task')
+    cy.contains('.btn', 'Añadir')
+        .click()
+    cy.get('#input-title')
+        .clear()
+
+    cy.get('#input-title')
+        .type('Other task')
+    cy.contains('.btn', 'Añadir')
+        .click()
+    cy.get('#input-title')
+        .clear()
+  })
+
+  it('Movind to lists view...', () => {
+
+      cy.get('.config-actions > .image-btn > img')
+        .click()
+  })
+})

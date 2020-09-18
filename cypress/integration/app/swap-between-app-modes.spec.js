2a104e2a0b2ffc3d61fe95312cc233d3106075cf
moises.alcocer <contacto@ironwoods.es>
2020-09-19 00:22:23 +0200

    added cypress test: swap view lists to tasks

---
+++ cypress/integration/app/swap-between-app-modes.spec.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+describe('Swap between the app\'s modes: lists and tasks views', () => {
+  it('Opening the ' + Cypress.config().appName + '...', () => {
+
+    cy.visit(Cypress.config().baseUrl)
+  })
+
+  it('Add lists', () => {
+
+    cy.get('#input-title')
+      .type('fake list')
+    cy.contains('.btn', 'Añadir')
+      .click()
+    cy.get('#input-title')
+      .clear()
+
+    cy.get('#input-title')
+      .type('My second list')
+    cy.get('#input-description')
+      .type('Description text')
+    cy.contains('.btn', 'Añadir')
+      .click()
+    cy.get('#input-title')
+      .clear()
+  })
+
+  it('Movind to tasks view...', () => {
+
+      cy.get('.config-actions > .image-btn > img')
+        .click()
+  })
+})

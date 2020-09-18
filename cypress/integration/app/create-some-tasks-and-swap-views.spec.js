7525890e203ad42a0e3b21b200d1b201ac0eaf53
moises.alcocer <contacto@ironwoods.es>
2020-09-19 01:04:40 +0200

    fixed cypress: create tasks & go back to lists
    was neccessary preserve local-storage data between specs

--- cypress/integration/app/create-some-tasks-and-swap-views.spec.js
+++ cypress/integration/app/create-some-tasks-and-swap-views.spec.js
@@ -1,5 +1,36 @@
 /// <reference types="cypress" />
 
+// Preserve local storage between specs:
+// https://blog.liplex.de/keep-local-storage-in-cypress/
+let LOCAL_STORAGE_MEMORY = {};
+
+Cypress.Commands.add("clearLocalStorageCache", () => {
+    localStorage.clear();
+    LOCAL_STORAGE_MEMORY = {};
+});
+Cypress.Commands.add("saveLocalStorageCache", () => {
+    Object.keys(localStorage).forEach(key => {
+        LOCAL_STORAGE_MEMORY[key] = localStorage[key];
+    });
+});
+Cypress.Commands.add("restoreLocalStorageCache", () => {
+    Object.keys(LOCAL_STORAGE_MEMORY).forEach(key => {
+        localStorage.setItem(key, LOCAL_STORAGE_MEMORY[key]);
+    });
+});
+
+afterEach(() => {
+    cy.saveLocalStorageCache();
+});
+before(function () {
+    cy.clearLocalStorageCache();
+});
+beforeEach(() => {
+    cy.restoreLocalStorageCache();
+});
+
+// Start tests
+
 describe('Swap between the app\'s modes: lists and tasks views', () => {
   it('Opening the ' + Cypress.config().appName + '...', () => {
 

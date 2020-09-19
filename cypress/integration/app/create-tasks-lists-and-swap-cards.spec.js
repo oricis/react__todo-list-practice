864559389f74b764dbecc42e129633c9cc1e752e
moises.alcocer <contacto@ironwoods.es>
2020-09-19 13:33:55 +0200

    cypress: create tasks on diferent lists

---
+++ cypress/integration/app/create-tasks-lists-and-swap-cards.spec.js
@@ -0,0 +1,138 @@
+/// <reference types="cypress" />
+
+// Preserve local storage between specs:
+// https://blog.liplex.de/keep-local-storage-in-cypress/
+let LOCAL_STORAGE_MEMORY = {};
+
+Cypress.Commands.add("clearLocalStorageCache", () => {
+    localStorage.clear();
+    LOCAL_STORAGE_MEMORY = {};
+});
+Cypress.Commands.add("saveLocalStorageCache", () => {
+    Object.keys(localStorage).forEach(key => {
+        LOCAL_STORAGE_MEMORY[key] = localStorage[key];
+    });
+});
+Cypress.Commands.add("restoreLocalStorageCache", () => {
+    Object.keys(LOCAL_STORAGE_MEMORY).forEach(key => {
+        localStorage.setItem(key, LOCAL_STORAGE_MEMORY[key]);
+    });
+});
+
+afterEach(() => {
+    cy.saveLocalStorageCache();
+});
+before(function () {
+    cy.clearLocalStorageCache();
+});
+beforeEach(() => {
+    cy.restoreLocalStorageCache();
+});
+
+// Start tests
+
+describe('Create lists & tasks and swap between cards', () => {
+  it('Opening the ' + Cypress.config().appName + '...', () => {
+
+    cy.visit(Cypress.config().baseUrl)
+  })
+
+  it('Add one list', () => {
+
+    cy.get('#input-title')
+      .type('fake list')
+    cy.contains('.btn', 'Añadir')
+      .click()
+    cy.get('#input-title')
+      .clear()
+  })
+
+  it('Moving to tasks view...', () => {
+
+      cy.get('.config-actions > .image-btn > img')
+        .click()
+  })
+
+  it('Adding tasks...', () => {
+
+    cy.get('#input-title')
+        .type('First task')
+    cy.contains('.btn', 'Añadir')
+        .click()
+    cy.get('#input-title')
+        .clear()
+
+    cy.get('#input-title')
+        .type('My second task')
+    cy.contains('.btn', 'Añadir')
+        .click()
+    cy.get('#input-title')
+        .clear()
+
+    cy.get('#input-title')
+        .type('Other task')
+    cy.contains('.btn', 'Añadir')
+        .click()
+    cy.get('#input-title')
+        .clear()
+  })
+
+  it('Moving to lists view...', () => {
+
+      cy.get('.config-actions > .image-btn > img')
+        .click()
+  })
+
+  it('Add other list and select', () => {
+
+    cy.get('#input-title')
+      .type('My second list')
+    cy.contains('.btn', 'Añadir')
+      .click()
+    cy.get('#input-title')
+      .clear()
+
+    cy.contains('.cards .card', 'My second list')
+      .contains('.btn', 'Seleccionar')
+      .click()
+  })
+
+  it('Moving to tasks view...', () => {
+
+    cy.get('.config-actions > .image-btn > img')
+      .click()
+  })
+
+  it('Adding task with auto-title...', () => {
+
+    cy.contains('.btn', 'Añadir')
+      .click()
+    cy.get('#input-title')
+      .clear()
+  })
+
+  it('Moving to lists view...', () => {
+
+    cy.get('.config-actions > .image-btn > img')
+      .click()
+  })
+
+  it('Change selected list card', () => {
+    cy.contains('.cards .card', 'fake list')
+      .contains('.btn', 'Seleccionar')
+      .click()
+  })
+
+  it('Moving to tasks view...', () => {
+
+    cy.get('.config-actions > .image-btn > img')
+      .click()
+  })
+
+  it('Delete the task labeled "My second task"', () => {
+
+    cy.contains('.cards .card', 'My second task')
+      .contains('.btn', 'Eliminar')
+      .click()
+  })
+})
